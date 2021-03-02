c3604685ffbddc1e48049aa402d9eb31c9f8c429
MP321515 <mpn1016@gmail.com>
2021-03-01 18:11:02 -0800

    Signed-off-by: MP321515 <mpn1016@gmail.com>

---
+++ Lab/Intro Lab(Marcos Placencia)/Intro_Lab/main.cpp
@@ -0,0 +1,21 @@
+/* 
+ * File:   main.cpp
+ * Author: Marcos Placencia
+ * Created on February 23, 2021, 1:26 PM
+ * Purpose: Hello World Program
+ */
+
+//System Libraries
+#include <iostream>     //I/O Library
+#include <cstdlib>
+using namespace std;
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Display Message
+    cout << "Hello, World!!";
+    
+    //Exit the Program - Cleanup
+    return 0;
+}
+
